c43adac2a9608509dcc8a69453aecf4c758fc786
LinYuMingTaipei <a828215362@gmail.com>
2020-12-07 15:02:52 +0800

    insert data to mongodb

--- hotel/pipelines.py
+++ hotel/pipelines.py
@@ -6,34 +6,27 @@
 # See: https://doc.scrapy.org/en/latest/topics/item-pipeline.html
 
 
-import pymysql
+from pymongo import MongoClient
+
 
 class HotelPipeline(object):
-    def __init__(self):
-        dbparams={
-            'host' : '127.0.0.1',
-            'port' : 3306,
-            'user' : 'root',
-            'password' : 'password',
-            'database' : 'Hotel',
-            'charset' : 'utf8mb4'
-        }
-        self.conn = pymysql.connect(**dbparams)
-        self.cursor = self.conn.cursor()
-        self._sql = None
+
+    def open_spider(self, spider):
+        db_uri = spider.settings.get('MONGODB_URI', 'mongodb://localhost:27017')
+        db_name = spider.settings.get('MONGODB_DB_NAME', 'booking')
+        self.db_client = MongoClient('mongodb://localhost:27017')
+        self.db = self.db_client[db_name]
+
 
     def process_item(self, item, spider):
-        params = (item['title'],item['image'],item['address'],item['score'],item['count'],item['landlord_img'],item['house_info'],item['secnery_list'],item['secnery_list2'],item["comment_list"],item["go_time"],item["exit_time"],item["facility"],item['room_type'])
-        self.cursor.execute(self.sql, params)
-        self.conn.commit()
+        self.insert_article(item)
         return item
 
-    @property
-    def sql(self):
-        if not self._sql:
-            self._sql="""
-            insert into booking(id, title, image, address, score, count, landlord_img, house_info, secnery_list, secnery_list2, comment_list, go_time, exit_time, facility, room_type)
-            values(null, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s)
-            """
-            return self._sql
-        return self._sql
+
+    def insert_article(self, item):
+        item = dict(item)
+        self.db.article.update({'title': item['title']}, {'$set': item}, upsert=True)
+
+
+    def close_spider(self, spider):
+        self.db_clients.close()
--- hotel/spiders/booking.py
+++ hotel/spiders/booking.py
@@ -2,112 +2,68 @@
 import scrapy
 from scrapy.spiders import CrawlSpider
 from hotel.items import HotelItem
+import re
+
 class BookingSpider(scrapy.Spider):
     name = 'booking'
     allowed_domains = ['www.booking.com']
-    base_domain = "https://www.booking.com"
-    urls = 'https://www.booking.com/{}'
-    cities = ["台北","新北","台中","高雄","北京","上海","東京","河內","曼谷","吉隆坡"]
-    start_urls = []
-    # 爬取亞洲各大城市的飯店
-    for city in cities:
-        start_urls.append(urls.format(city))
+    base_domain = 'https://www.booking.com'
+    cities = ['台北', '新北', '台中', '高雄', '北京', '上海', '東京', '河內', '曼谷', '吉隆坡']
     
+    start_urls = [f'https://www.booking.com/{city}' for city in cities]
     
 
     def parse(self, response):
-        # 分析詳情頁面url 以及 下一頁的url
         info_urls = response.xpath('//a[@class=" sr_item_photo_link sr_hotel_preview_track  "]/@href').getall()
         next_urls = response.xpath('//nav/ul/li[3]/a/@href').get()
         for info_url in info_urls:
-            yield scrapy.Request(self.base_domain+info_url, callback=self.parse_info)
+            yield scrapy.Request(self.base_domain + info_url, callback=self.parse_info)
 
-        yield scrapy.Request(self.base_domain+next_urls, callback=self.parse)
+        yield scrapy.Request(self.base_domain + next_urls, callback=self.parse)
+
+
+    def parseTourist(self, response):
+        tourists = []
+        touristInfo = response.xpath("//li[@class='bui-list__item']")
+        for t in touristInfo:
+            row = {}
+            row['tourist']  = t.xpath("./div/div/text()").getall()[0].strip()
+            row['distance'] = t.xpath("./div/div/text()").getall()[1].strip()
+            tourists.append(row)
+        return tourists
+
 
     def parse_info(self, response):
-        # 飯店資訊
-        title = response.xpath('//h2[@class="hp__hotel-name"]/text()').getall()[1].replace("\n","")
-        image = response.xpath('//div[@id="photos_distinct"]/a/@href').getall()
-        image = ",".join(image)
+        tourists = self.parseTourist(response)
         address = response.xpath("//p[@class='address address_clean']/span/text()").get().strip()
-        
-        score = response.xpath("//div[@class='bui-review-score__badge']/text()").get()
-        if score:
-            score = float(score.split(" ")[1])
-        else:
-            score = float(score)
-        
-        count = response.xpath('//div[@class="bui-review-score__text"]/text()').get().split(" ")[1]
-        if "," in count:
-            count = count.split(",")
-            count = int("".join(count))
-        count = int(count)
 
-        landlord_img = response.xpath('//img[@class="bui-avatar__image hops__host-photo js-host-info__photo js-host-info__photo--photo"]/@src').get()
+        title = response.xpath('//h2[@id="hp_hotel_name"]/text()').getall()[1].strip()
+        image = response.xpath('//a[@target="_blank"]/@href').getall()
+        comments = response.xpath("//span[@class='c-review__body']/text()").getall()
         
-        house_info = response.xpath('//div[@id="property_description_content"]/p//text()').getall()
-        house_info = ",".join(house_info)
-        # 附近人文景點
-        secnery = response.xpath('//ul[@class="bui-list bui-list--text hp-poi-list__wrapper"]/li/div/div/text()').getall()
-        distance = response.xpath('//ul[@class="bui-list bui-list--text hp-poi-list__wrapper"]/li/div/span/text()').getall()
-        secnery_list = list()
+        facilities = response.xpath("//div[@class='hp_desc_important_facilities clearfix hp_desc_important_facilities--bui ']/div/text()").getall()
+        facilities = [i.strip() for i in list(filter(lambda x: x != '\n', facilities))]
 
-        for tourism in zip(secnery,distance):
-            secnery=tourism[0].replace("\n","")
-            distance=tourism[1].replace("\n","")
-            tourism=secnery,distance
-            secnery_list.append(tourism)
+        description = response.xpath("//div[@id='property_description_content']/p/text()").getall()
+        description = ''.join(description)
         
-        secnery_list = str(secnery_list).split("[")[1].split("]")[0].replace("(","").replace(")","|").replace("'","").replace(",","")
+        stars = response.xpath("//div[@class='bui-review-score c-score']/div[@class='bui-review-score__badge']/text()").get()
+        stars = stars[0] if stars else 0 
         
-        # 附近自然景點
-        secnery_list2 = list()
-        landscape = response.xpath('//ul[@class="bui-list bui-list--text add hp-poi-list__wrapper"]/li/div/div/span[1]/text()').getall()
-        distance = response.xpath('//ul[@class="bui-list bui-list--text add hp-poi-list__wrapper"]/li/div/span/text()').getall()
-        for tourism in zip(landscape,distance):
-            secnery=tourism[0].replace("\n","")
-            distance=tourism[1].replace("\n","")
-            tourism=secnery,distance
-            secnery_list2.append(tourism)
-        
-        secnery_list2 = str(secnery_list2).split("[")[1].split("]")[0].replace("(","").replace(")","|").replace("'","").replace(",","")
+        ratings = response.xpath("//span[@class='hp__hotel_ratings']/span/i/@title").get()
+        ratings = ratings[0] if ratings else 0
 
-        # 爬取評論
-        comment_list = []
-        selectors = response.xpath('//div[@class="reviews-snippet-sidebar__item"]')
-        for selector in selectors:
-            img = selector.xpath('.//img/@src').get()
-            author = selector.xpath('.//span[@class="bui-avatar-block__title"]/text()').get()
-            comment = selector.xpath('.//span[@class="c-review__body"]/text()').get()
-            comment_info = img,author,comment
-            comment_list.append(comment_info)
-        comment_list = str(comment_list).split("[")[1].split("]")[0].replace("(","").replace(")","|").replace("'","").replace(",","")
+        bed_type = response.xpath("//div[@class='room-info']/a/text()").getall()
+        bed_type = [i.strip() for i in list(filter(lambda x: x != '\n', bed_type))]         
 
-        # 入住、退宿時間
         time = response.xpath('//span[@class="u-display-block"]/text()').getall()
-        go_time = time[0].replace("\n","")
-        exit_time = time[1].replace("\n","")
+        checkin_time  = re.sub('[\u4e00-\u9fa5]', '', time[0].strip())
+        checkout_time  = re.sub('[\u4e00-\u9fa5]', '', time[1].strip())
         
-        #  飯店設施
-        facility = response.xpath('//div[1][@class="hp_desc_important_facilities clearfix "]/div/text()').getall()[1::2]
-        facility_list=[]
-        for f in facility:
-            facility_list.append(f.replace("\n",""))
-        facility_list = str(facility_list).split("[")[1].split("]")[0].replace("(","").replace(")","|").replace("'","").replace(",","")
-        
-        # 房型
-        selectors = response.xpath('//div[@class="room-info"]')
-        room_list = list()
-        for selector in selectors:
-            try:
-                room_type = selector.xpath('.//a[@class="jqrt togglelink"]/text()').getall()[-1].replace("\n","")
-                room_type1 = selector.xpath('.//li[@class="rt-bed-type"]/span/text()').getall()[0].replace("\n","")
-                room = room_type,room_type1
-                room_list.append(room)
-            except:
-                continue
-        room_type = str(room_list).split("[")[1].split("]")[0].replace("(","").replace(")","|").replace("'","").replace(",","")
-
-        item = HotelItem(title=title,address=address,score=score,count=count,landlord_img=landlord_img,house_info=house_info,image=image,secnery_list=secnery_list, \
-        secnery_list2=secnery_list2,comment_list=comment_list,go_time=go_time,exit_time=exit_time,facility=facility_list,room_type=room_type)
+        row = {
+            'title': title, 'image': image, 'comments': comments, \
+            'address': address, 'facilities': facilities, 'description': description, 'stars': stars,\
+            'ratings': ratings, 'bed_type': bed_type, 'checkin_time': checkin_time, 'checkout_time': checkout_time
+        }
+        item = HotelItem(row)
         return item
